71f0337110b7a7b8bc7fd7e59a88af48cf947814
박용빈 <pyb0718@naver.com>
2024-07-25 14:47:01 +0900

    :page_facing_up: initial commit

---
+++ BE/src/test/java/com/B2A4/storybook/StorybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.B2A4.storybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
