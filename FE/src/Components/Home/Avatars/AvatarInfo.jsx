6fa6c049529901832a517c54dc3ba6ada4546e33
01 <bubbleutopia@naver.com>
2024-07-29 15:48:22 +0900

    :page_facing_up: : Avatars 구조 구현 완료

---
+++ FE/src/Components/Home/Avatars/AvatarInfo.jsx
@@ -0,0 +1,16 @@
+import AvatarExp from './AvatarExp'
+import './css/AvatarInfo.css'
+
+function AvatarInfo() {
+  return (
+    <div className='avatarinfo'>
+      <h3>내 동그리</h3>
+      <div className='infodetail'>
+        <img src="/img/avatars/geyomi2.png" alt="avatar" />
+        <AvatarExp />
+      </div>
+    </div>
+  )
+}
+
+export default AvatarInfo
