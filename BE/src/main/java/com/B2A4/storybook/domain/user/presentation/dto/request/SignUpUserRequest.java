6c303a783947cec228cb8fb515b204fe00be0a27
박용빈 <pyb0718@naver.com>
2024-08-01 13:57:05 +0900

    :art: 코드 정렬 작업

--- BE/src/main/java/com/B2A4/storybook/domain/user/presentation/dto/request/SignUpUserRequest.java
+++ BE/src/main/java/com/B2A4/storybook/domain/user/presentation/dto/request/SignUpUserRequest.java
@@ -2,5 +2,11 @@ package com.B2A4.storybook.domain.user.presentation.dto.request;
 
 import com.B2A4.storybook.domain.oauth.domain.OauthServerType;
 
-public record SignUpUserRequest(String name, String email, String nickname, String profileImageUrl, OauthServerType oauthServerType) {
+public record SignUpUserRequest(
+        String name,
+        String email,
+        String nickname,
+        String profileImageUrl,
+        OauthServerType oauthServerType
+) {
 }
