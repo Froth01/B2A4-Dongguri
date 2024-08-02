541a4378d204f4631e985dad738912855118ac72
박용빈 <pyb0718@naver.com>
2024-08-02 17:25:54 +0900

    :page_facing_up: 동화 Service 추가

---
+++ BE/src/main/java/com/B2A4/storybook/domain/storybook/service/StorybookService.java
@@ -0,0 +1,60 @@
+package com.B2A4.storybook.domain.storybook.service;
+
+import com.B2A4.storybook.domain.keyword.domain.Keyword;
+import com.B2A4.storybook.domain.keyword.domain.repository.KeywordRepository;
+import com.B2A4.storybook.domain.storybook.domain.Storybook;
+import com.B2A4.storybook.domain.storybook.domain.repository.StorybookRepository;
+import com.B2A4.storybook.domain.storybook.exception.StorybookNotFoundException;
+import com.B2A4.storybook.domain.storybook.presentation.dto.request.CreateStorybookRequest;
+import com.B2A4.storybook.domain.storybook.presentation.dto.response.StorybookResponse;
+import com.B2A4.storybook.domain.user.domain.User;
+import com.B2A4.storybook.global.utils.user.UserUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StorybookService implements StorybookServiceUtils {
+
+    private final StorybookRepository storybookRepository;
+    private final KeywordRepository keywordRepository;
+    private final UserUtils userUtils;
+
+    @Transactional
+    public StorybookResponse createStorybook(CreateStorybookRequest createStorybookRequest) {
+        User user = userUtils.getUserFromSecurityContext();
+        String transparentImageUrl = "투명 이미지 URL";
+        String voiceRecordingUrl = "녹음 파일 URL";
+        Storybook storybook = Storybook.createStorybook(
+                user,
+                createStorybookRequest.genre(),
+                createStorybookRequest.content(),
+                createStorybookRequest.originalImageUrl(),
+                createStorybookRequest.transformedImageUrl(),
+                transparentImageUrl,
+                voiceRecordingUrl,
+                createStorybookRequest.isTodayKeyword()
+        );
+
+        List<String> keywords = createStorybookRequest.keyword();
+
+        for (String keyword : keywords) {
+            Keyword keywordEntity = Keyword.createKeyword(storybook, keyword);
+            keywordRepository.save(keywordEntity);
+        }
+
+//        return new  StorybookResponse(storybook.getStorybookInfoVO(storybook, keywords))
+        return null;
+    }
+
+    @Override
+    public Storybook queryStorybook(Long storybookId) {
+        return storybookRepository.findById(storybookId).orElseThrow(() -> StorybookNotFoundException.EXCEPTION);
+    }
+}
---
+++ BE/src/main/java/com/B2A4/storybook/domain/storybook/service/StorybookServiceUtils.java
@@ -0,0 +1,7 @@
+package com.B2A4.storybook.domain.storybook.service;
+
+import com.B2A4.storybook.domain.storybook.domain.Storybook;
+
+public interface StorybookServiceUtils {
+    Storybook queryStorybook(Long storybookId);
+}
