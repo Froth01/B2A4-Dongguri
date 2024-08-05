3140b6f47c3a6463a8aa2769ac668d502abd0495
01 <bubbleutopia@naver.com>
2024-08-05 17:45:19 +0900

    :sparkles: : myworld 기능 구현, 기타 코드 수정

--- FE/src/Components/Home/StoryWorld/MyWorld.jsx
+++ FE/src/Components/Home/StoryWorld/MyWorld.jsx
@@ -1,13 +1,46 @@
-import './css/MyWorld.css'
+import { useEffect, useRef } from 'react';
+import './css/MyWorld.css';
 
-function MyWorld() {
+const MyWorld = () => {
+  const myWorldRef = useRef(null);
+
+  useEffect(() => {
+    const randomPosition = (element) => {
+      if (!myWorldRef.current) return;
+
+      const { clientWidth, clientHeight } = myWorldRef.current;
+      const { offsetWidth, offsetHeight } = element;
+
+      // 이미지가 화면 밖으로 나가지 않도록 조정
+      const x = Math.floor(Math.random() * 1000);
+      const y = Math.floor(Math.random() * 500);
+      element.style.transform = `translate(${x}px, ${y}px)`;
+    };
+
+    const animateImages = () => {
+      const images = myWorldRef.current.querySelectorAll('.floating-image');
+      images.forEach((image) => {
+        randomPosition(image);
+      });
+    };
+
+    animateImages(); // 초기 위치 설정
+    const intervalId = setInterval(animateImages, 5000); // 5초마다 위치 변경
+
+    // 컴포넌트가 언마운트될 때 인터벌 정리
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className='myworld'>
-      <div className='testmyworld'>
-        testmyworld
-      </div>
+    <div className="myworld" ref={myWorldRef}>
+      <img src="/img/avatars/bini_ONE.png" className="floating-image" alt="이미지 1" />
+      <img src="/img/avatars/geyomi_ONE.png" className="floating-image" alt="이미지 2" />
+      <img src="/img/avatars/geyomi_ONE.png" className="floating-image" alt="이미지 3" />
+      <img src="/img/avatars/geyomi_ONE.png" className="floating-image" alt="이미지 4" />
+      <img src="/img/avatars/geyomi_ONE.png" className="floating-image" alt="이미지 5" />
+      <img src="/img/avatars/geyomi_ONE.png" className="floating-image" alt="이미지 6" />
     </div>
-  )
-}
+  );
+};
 
-export default MyWorld
+export default MyWorld;
