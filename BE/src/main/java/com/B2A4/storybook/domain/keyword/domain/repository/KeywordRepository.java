11aa2dbbe23fc3f7df5bfe3ef4607ddeee900531
박용빈 <pyb0718@naver.com>
2024-08-05 23:18:10 +0900

    :sparkles: 동화 키워드로 조회 추가

--- BE/src/main/java/com/B2A4/storybook/domain/keyword/domain/repository/KeywordRepository.java
+++ BE/src/main/java/com/B2A4/storybook/domain/keyword/domain/repository/KeywordRepository.java
@@ -3,5 +3,8 @@ package com.B2A4.storybook.domain.keyword.domain.repository;
 import com.B2A4.storybook.domain.keyword.domain.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    List<Keyword> findByKeyword(String keyword);
 }
