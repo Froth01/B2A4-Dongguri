a0b1c666ab45318b2c6046b7d823e57b3d79936c
박용빈 <pyb0718@naver.com>
2024-08-05 23:07:56 +0900

    :sparkles: 동화 기능 추가

--- BE/src/main/java/com/B2A4/storybook/domain/storybook/service/StorybookServiceUtils.java
+++ BE/src/main/java/com/B2A4/storybook/domain/storybook/service/StorybookServiceUtils.java
@@ -3,5 +3,15 @@ package com.B2A4.storybook.domain.storybook.service;
 import com.B2A4.storybook.domain.storybook.domain.Storybook;
 
 public interface StorybookServiceUtils {
+    StorybookResponse createStorybook(CreateStorybookRequest createStorybookRequest);
+
+    StorybookResponse getStorybook(Long storybookId);
+
+    List<StorybookResponse> getStorybookListByUser(long userId);
+
+    List<StorybookResponse> getStorybookListByKeyword(String keyword);
+
+    void deleteStorybook(Long storybookId);
+
     Storybook queryStorybook(Long storybookId);
 }
