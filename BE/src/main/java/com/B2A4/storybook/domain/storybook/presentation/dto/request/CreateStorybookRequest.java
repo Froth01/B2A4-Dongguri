f2ac2b72ca4cddb8d184afc8ded565d5d7f89458
이건희 <022686s@gmail.com>
2024-08-07 09:38:18 +0900

    :art: 녹음 파일 타입 수정

--- BE/src/main/java/com/B2A4/storybook/domain/storybook/presentation/dto/request/CreateStorybookRequest.java
+++ BE/src/main/java/com/B2A4/storybook/domain/storybook/presentation/dto/request/CreateStorybookRequest.java
@@ -11,7 +11,7 @@ public record CreateStorybookRequest(
         String content,
         String originalImageUrl,
         String transformedImageUrl,
-        MultipartFile voiceRecordingFile,
+        String voiceRecordingUrl,
         boolean isTodayKeyword
 ) {
 }
