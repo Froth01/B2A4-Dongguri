9e12205aa0c701b901a09f924466264c6002aa90
sunmin <bbu0330@gmail.com>
2024-08-08 23:00:05 +0900

    :art: : userserach fixing

--- FE/src/Components/Home/SNS/SNS.jsx
+++ FE/src/Components/Home/SNS/SNS.jsx
@@ -6,22 +6,21 @@ import { useSelector } from 'react-redux'
 
 function SNS() {
   const searchResults = useSelector(selectResults)
-  console.log('검색결과',searchResults)
+  console.log('검색결과', searchResults)
 
   const searchUserResults = useSelector(selectUserResults)
-  console.log('유저검색',searchUserResults)
+  console.log('유저검색', searchUserResults)
 
-  const searchType = useSelector(selectSearchType);
-  console.log('검색 타입',searchType)
+  const searchType = useSelector(selectSearchType)
+  console.log('검색 타입', searchType)
 
   return (
     <div className='sns'>
       <SearchBar />
-      {searchType === 'storybook' && <MiniCardList cardList={searchResults.data} />}
+      {searchType === 'storybook' && searchResults.data && <MiniCardList cardList={searchResults.data} />}
       {searchType === 'user' && searchUserResults && <MiniCardList cardList={[searchUserResults]} />}
-      {/* <MiniCardList cardList={searchResults.data} /> */}
     </div>
-  );
+  )
 }
 
 export default SNS;
