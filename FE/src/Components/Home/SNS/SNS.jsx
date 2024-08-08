78cb29d4410810160f66e0bc2b7e8227d48a533f
sunmin <bbu0330@gmail.com>
2024-08-08 16:40:58 +0900

    재업로드

--- FE/src/Components/Home/SNS/SNS.jsx
+++ FE/src/Components/Home/SNS/SNS.jsx
@@ -2,11 +2,16 @@ import MiniCardList from '../Common/MiniCardList'
 import SearchBar from './SearchBar'
 import './css/SNS.css'
 import { selectResults } from '../../../slices/searchSlice'
+import { selectUserId } from '../../../slices/searchUserSlice'
 import { useSelector } from 'react-redux'
 
 function SNS() {
   const searchResults = useSelector(selectResults)
   console.log('검색결과',searchResults)
+
+  const searchUser = useSelector(selectUserId)
+  console.log('유저검색',searchUser)
+
   return (
     <div className='sns'>
       <SearchBar />
