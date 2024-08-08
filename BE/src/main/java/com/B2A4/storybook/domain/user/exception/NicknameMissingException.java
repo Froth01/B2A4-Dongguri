e15b89760a82404e9b05cba7dfec44cbbe0cfb6b
박용빈 <pyb0718@naver.com>
2024-08-08 09:00:18 +0900

    :page_facing_up: 예외 추가

---
+++ BE/src/main/java/com/B2A4/storybook/domain/user/exception/NicknameMissingException.java
@@ -0,0 +1,13 @@
+package com.B2A4.storybook.domain.user.exception;
+
+import com.B2A4.storybook.global.error.exception.ErrorCode;
+import com.B2A4.storybook.global.error.exception.MainException;
+
+public class NicknameMissingException  extends MainException {
+
+    public static final MainException EXCEPTION = new NicknameMissingException ();
+
+    private NicknameMissingException () {
+        super(ErrorCode.NICKNAME_MISSING);
+    }
+}
