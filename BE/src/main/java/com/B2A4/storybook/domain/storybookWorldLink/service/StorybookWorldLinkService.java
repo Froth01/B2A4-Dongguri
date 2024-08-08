06ac6391ac2f83f4b186ae5b26c1a598de08b037
박용빈 <pyb0718@naver.com>
2024-08-08 11:06:04 +0900

    :recycle:  동화나라 Response 수정

--- BE/src/main/java/com/B2A4/storybook/domain/storybookWorldLink/service/StorybookWorldLinkService.java
+++ BE/src/main/java/com/B2A4/storybook/domain/storybookWorldLink/service/StorybookWorldLinkService.java
@@ -1,11 +1,14 @@
 package com.B2A4.storybook.domain.storybookWorldLink.service;
 
 import com.B2A4.storybook.domain.storyWorld.domain.StoryWorld;
+import com.B2A4.storybook.domain.storyWorld.presentation.dto.response.StoryWorldStorybookResponse;
 import com.B2A4.storybook.domain.storybook.domain.Storybook;
-import com.B2A4.storybook.domain.storybook.domain.vo.StorybookInfoVO;
+import com.B2A4.storybook.domain.storybook.exception.UserNotStorybookHostException;
 import com.B2A4.storybook.domain.storybook.service.StorybookService;
 import com.B2A4.storybook.domain.storybookWorldLink.domain.StorybookWorldLink;
 import com.B2A4.storybook.domain.storybookWorldLink.domain.repository.StorybookWorldLinkRepository;
+import com.B2A4.storybook.domain.user.domain.User;
+import com.B2A4.storybook.global.utils.user.UserUtilsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,12 +23,21 @@ public class StorybookWorldLinkService implements StorybookWorldLinkServiceUtils
 
     private final StorybookWorldLinkRepository storybookWorldLinkRepository;
     private final StorybookService storybookService;
+    private final UserUtilsImpl userUtilsImpl;
 
     @Transactional
     @Override
-    public List<StorybookInfoVO> createStorybookWorldLink(StoryWorld storyWorld, List<Long> storybookIds) {
+    public List<StoryWorldStorybookResponse> createStorybookWorldLink(StoryWorld storyWorld, List<Long> storybookIds) {
+
+        User user = userUtilsImpl.getUserFromSecurityContext();
 
         List<Storybook> storybookList = storybookService.getStorybookListByStorybookIds(storybookIds);
+
+        for (Storybook storybook : storybookList) {
+            if (!storybook.getUser().getId().equals(user.getId())) {
+                throw UserNotStorybookHostException.EXCEPTION;
+            }
+        }
 
         List<StorybookWorldLink> storyWorldLinks = storybookList.stream()
                 .map(storybook -> StorybookWorldLink.createStorybook(storybook, storyWorld))
@@ -35,11 +47,14 @@ public class StorybookWorldLinkService implements StorybookWorldLinkServiceUtils
 
         storybookWorldLinkRepository.saveAll(storyWorldLinks);
 
-        return storybookList.stream().map(Storybook::getStorybookInfoVO).toList();
+        return storybookList.stream()
+                .map(Storybook::getStorybookInfoVO)
+                .map(storybookInfoVO -> new StoryWorldStorybookResponse(storybookInfoVO.storybookId(), storybookInfoVO.transparentImageUrl()))
+                .toList();
     }
 
     @Override
-    public List<StorybookInfoVO> getStorybookList(StoryWorld storyWorld) {
+    public List<StoryWorldStorybookResponse> getStorybookList(StoryWorld storyWorld) {
 
         List<StorybookWorldLink> storybookWorldLinks = storybookWorldLinkRepository.findAllByStoryWorld(storyWorld);
 
@@ -49,6 +64,9 @@ public class StorybookWorldLinkService implements StorybookWorldLinkServiceUtils
 
         List<Storybook> storybookList = storybookService.getStorybookListByStorybookIds(storybookIds);
 
-        return storybookList.stream().map(Storybook::getStorybookInfoVO).toList();
+        return storybookList.stream()
+                .map(Storybook::getStorybookInfoVO)
+                .map(storybookInfoVO -> new StoryWorldStorybookResponse(storybookInfoVO.storybookId(), storybookInfoVO.transparentImageUrl()))
+                .toList();
     }
 }
