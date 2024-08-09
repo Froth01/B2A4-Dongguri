5d3e39f384d8e569f33e1745e164997455099362
박용빈 <pyb0718@naver.com>
2024-08-09 12:42:09 +0900

    :wastebasket: 사용하지 않는 코드 삭제

--- BE/src/main/java/com/B2A4/storybook/domain/keyword/domain/repository/KeywordRepository.java
+++ BE/src/main/java/com/B2A4/storybook/domain/keyword/domain/repository/KeywordRepository.java
@@ -1,12 +1,7 @@
 package com.B2A4.storybook.domain.keyword.domain.repository;
 
 import com.B2A4.storybook.domain.keyword.domain.Keyword;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    Slice<Keyword> findByKeyword(String keyword, Pageable pageable);
 }
