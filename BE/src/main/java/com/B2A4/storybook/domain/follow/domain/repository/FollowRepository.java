7cefd8a2c2fe3b16746d0cb77e2b96d4c73f0b5c
박용빈 <pyb0718@naver.com>
2024-08-10 16:51:59 +0900

    :page_facing_up: 팔로우 Repository 생성

---
+++ BE/src/main/java/com/B2A4/storybook/domain/follow/domain/repository/FollowRepository.java
@@ -0,0 +1,19 @@
+package com.B2A4.storybook.domain.follow.domain.repository;
+
+import com.B2A4.storybook.domain.follow.domain.Follow;
+import com.B2A4.storybook.domain.user.domain.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Slice<Follow> findFollowersByFollowing(User following, Pageable pageable);
+
+    Slice<Follow> findFollowingsByFollower(User follower, Pageable pageable);
+
+    boolean existsByFollowerAndFollowing(User follower, User following);
+
+    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
+}
