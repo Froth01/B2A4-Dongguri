6726fb3b1ea68768df2110f55c10203537a8d9b3
이건희 <022686s@gmail.com>
2024-08-13 10:42:00 +0900

    :sparkles: 댓글 작성자 정보 추가

--- BE/src/main/java/com/B2A4/storybook/domain/comment/presentation/dto/response/CommentResponse.java
+++ BE/src/main/java/com/B2A4/storybook/domain/comment/presentation/dto/response/CommentResponse.java
@@ -1,18 +1,22 @@
 package com.B2A4.storybook.domain.comment.presentation.dto.response;
 
 import com.B2A4.storybook.domain.comment.domain.vo.CommentInfoVO;
+import com.B2A4.storybook.domain.user.domain.vo.UserInfoVO;
 
 import java.time.LocalDateTime;
 
 public record CommentResponse(
         Long commentId,
-        Long userId,
         Long storybookId,
         String comment,
-        LocalDateTime created
+        LocalDateTime created,
+        Long userId,
+        String nickname,
+        String profileImageUrl
+
 ) {
-    public CommentResponse(CommentInfoVO commentInfoVO) {
-        this(commentInfoVO.commentId(), commentInfoVO.userId(), commentInfoVO.storybookId(), commentInfoVO.comment()
-        , commentInfoVO.created());
+    public CommentResponse(CommentInfoVO commentInfoVO, UserInfoVO userInfoVO) {
+        this(commentInfoVO.commentId(), commentInfoVO.storybookId(), commentInfoVO.comment()
+        , commentInfoVO.created(), userInfoVO.userId(), userInfoVO.nickname(), userInfoVO.profileImageUrl());
     }
 }
