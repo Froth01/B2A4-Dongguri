fd26fcd17308328f5dbff6dedf622d0a7833c17c
01 <bubbleutopia@naver.com>
2024-08-13 21:59:42 +0900

    :art: : 네브바 유저이미지 로그인시 작게표시

--- FE/src/Components/Home/Users/KakaoLoginButton.jsx
+++ FE/src/Components/Home/Users/KakaoLoginButton.jsx
@@ -1,5 +1,6 @@
 
 import { useEffect } from 'react';
+import './css/KakaoLoginButton.css'
 
 const KakaoLoginButton = () => {
   useEffect(() => {
@@ -28,7 +29,11 @@ const KakaoLoginButton = () => {
   };
 
   return (
-    <div>
+    <div className='kakaologin'>
+      <div className="welcome">
+        동그리에 오신 것을 환영합니다!!<br></br><br></br>
+        카카오 아이디로 로그인해주세요!
+      </div>
       <button id="kakao-login-btn" onClick={loginWithKakao}>
         <img src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg" width="222" alt="카카오 로그인 버튼" />
       </button>
