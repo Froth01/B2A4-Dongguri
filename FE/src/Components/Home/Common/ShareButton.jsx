02f22c2e6a189cb9fb46831dd99ef9f84e9615d3
sunmin <bbu0330@gmail.com>
2024-08-15 21:38:41 +0900

    :bug: :공유버튼 키 변경

--- FE/src/Components/Home/Common/ShareButton.jsx
+++ FE/src/Components/Home/Common/ShareButton.jsx
@@ -17,8 +17,8 @@ const KakaoShareButton = ({ card }) => {
           script.crossOrigin = 'anonymous';
           script.onload = () => {
             if (window.Kakao && !window.Kakao.isInitialized()) {
-              window.Kakao.init('65c79327b948613354ca939c2c67db0f'); // JavaScript 키
-              // window.Kakao.init('0536894c82d8dda2013ee7211364733a'); // (서버키)JavaScript 키
+              // window.Kakao.init('65c79327b948613354ca939c2c67db0f'); // JavaScript 키
+              window.Kakao.init('0536894c82d8dda2013ee7211364733a'); // (서버키)JavaScript 키
             }
             resolve();
           };
